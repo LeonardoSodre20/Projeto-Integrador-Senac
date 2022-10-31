90de41e76bcd4e70f12224961142ebed9faa60d6
LEONARDO VIEIRA SODRE <leonardo.vsodre@senacsp.edu.br>
2022-10-31 11:36:57 -0300

    Em andamento

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+const btnMobile = document.querySelector(".btn");
+const listMenu = document.querySelector(".lista-de-navegacao");
+
+btnMobile.addEventListener("click", () => {
+  btnMobile.classList.toggle("active");
+  listMenu.classList.toggle("activeMenu");
+});
