45ef81ec3a7c91743f48db4d5d5a9cc251f102ca
Leonardo Vieira Sodré <LeonardoSodre25@outlook.com>
2022-11-08 11:44:17 -0300

    Projeto em andamento

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,15 @@
 const btnMobile = document.querySelector(".btn");
 const listMenu = document.querySelector(".lista-de-navegacao");
-
+const btnTop = document.querySelector(".container-btn-top");
 
 btnMobile.addEventListener("click", () => {
   btnMobile.classList.toggle("active");
   listMenu.classList.toggle("activeMenu");
 });
 
-
+btnTop.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
